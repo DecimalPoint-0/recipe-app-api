c92d3f11994eca57425e4b83ffe85e14613e43e8
decimalpoint <zubairuabduljelil2017@gmail.com>
2024-04-09 12:24:38 -0700

    Added more configuration

---
+++ app/test.py
@@ -0,0 +1,12 @@
+""" Sample test """
+from django.test import SimpleTestCase
+from . import calc
+
+class CalcTest(SimpleTestCase):
+    """ Test the calc functions """
+
+    def test_add_numbers(self):
+        """ test for adding numbers """
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
